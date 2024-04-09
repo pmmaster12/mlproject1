952ab9ccfac73fec01e9981f7acbb2fcdcd166e9
pmmaster12 <120505111+pmmaster12@users.noreply.github.com>
2024-04-09 23:42:55 +0530

    logging updated

--- src/exception.py
+++ src/exception.py
@@ -22,14 +22,7 @@ class CustomException(Exception):
    
     
 
-if __name__=="__main__":
-    
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("divisible by zero")
-        raise CustomException(e,sys)
-       
+
         
         
         
