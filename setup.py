8ffabeee5f41441044e2f864817fd3fa2ca67f98
pmmaster12 <120505111+pmmaster12@users.noreply.github.com>
2024-04-09 23:38:08 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+        
+        
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -23,5 +25,7 @@ author='Tarun',
 author_email='rajeshguptamsk12@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
+
+
 
 )
